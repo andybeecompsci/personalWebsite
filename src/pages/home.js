08b2c024c00db76cabf96de492f100cf3a6bd6d8
Anderson Bee <n10791051@qut.edu.au>
2024-11-18 12:20:35 +1000

    added reactivity to project selection  - super basic  - need to style  - functionality and idea is there tho

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,5 +1,5 @@
 //imports
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "reactstrap";
 import { useNavigate } from "react-router-dom";
 
@@ -12,41 +12,17 @@ export default function Home() {
 
     const navigate = useNavigate();
 
-    // return (
-    //     <div>
-    //         <NavBarTest />
-    //         <div style={{ backgroundColor: '#1B1818', color: 'white', minHeight: '100vh' }}>
+    const [selectedProject, setSelectedProject] = useState(null) //tracks selected project
 
+    //projects to be selected
+    const projects = [
+        { id:1, name: "Project 1", desciption: "this is project 1"},
+        { id: 2, name: "Project 2", desciption: "this is project 2" },
+        { id: 3, name: "Project 3", desciption: "this is project 3" },
+    ];
+    
 
-    //             <div style={{ display: 'flex' }}>
-
-    //                 <div style={{ flex: 1, textAlign: "center", marginTop: "400px" }}>
-    //                     <h1>welcome!</h1>
-    //                     <p>I'm currently a student at QUT, studying IT, Majoring in Computer Science, and Minoring in Network Security and Business Enterprise Systems</p>
-    //                     <Button
-    //                         color="dark"
-    //                         size="sm"
-    //                         className="mt-3"
-    //                         onClick={() => navigate("/personal")}>
-    //                         ABOUT ME 
-    //                     </Button>
-    //                 </div>
-
-    //                 <div style={{ flex: 2 }}>
-    //                     <p style={{ height: 750, width: 1250, marginTop: "140px", marginRight: '10px', marginLeft: '200px' }}> PROJECTS! CHOOSE A PROJECT
-    //                         ____________________________________________________
-    //                         LEARN HOW TO MAKE THESE CONTAINERS VERTICALLY SEPERATE, ADD INDIVIDUAL PROJECT DESCRIPTIONS LIKE EXAMPLE WEBSITE</p>
-                        
-    //                 </div>
-
-                  
-
-
-    //             </div>
-    //         </div>
-    //     </div>
-    // )
-
+    
     return (
         <div style={{ backgroundColor: '#1B1818', color: 'white', minHeight: '100vh' }}>
             <NavBarTest />
@@ -77,17 +53,31 @@ export default function Home() {
                     <section className="projects">
                         <h2>Selected Work</h2>
                     </section>
-
+                    {/* select project */}
                     <section className="proj_selection">
-                        <p>choose projects, updates section below, make these links</p>
+                        {projects.map((project) =>(
+                            <button
+                            key={project.id}
+                            className="proj_button"
+                            onClick={() => setSelectedProject(project)}
+                            >
+                                {project.name}
+                            </button>
+                        ))}
                     </section>
 
                     <section className="proj_details">
-                        <img alt="Solaire" src="/images/solaire_icon.png" className="proj_img" />
+                        {selectedProject ? (
+                            <div>
+                                <h2>{selectedProject.name}</h2>
+                                <p>{selectedProject.desciption}</p>
+                            </div>
+                        ) : (
+                            <p> select a project</p>
+                        )}
                     </section>
 
                     <section className="proj_details">
-                        <p>make this section reactive. loads content based off what is clicked on above</p>
                     </section>
                     
                 </div>
